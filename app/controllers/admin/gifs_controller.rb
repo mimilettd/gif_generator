ad84d1ccb68273d14fcdfe722fce58caa0922ccc
mimilettd <mimilettd@gmail.com>
2017-09-08 16:18:06 -0600

    generate random gif by search term

--- app/controllers/admin/gifs_controller.rb
+++ app/controllers/admin/gifs_controller.rb
@@ -8,11 +8,11 @@ class Admin::GifsController < ApplicationController
 
   def create
     search_term = gif_params[:search_term]
-    url = "http://api.giphy.com/v1/gifs/search?q=#{search_term}&api_key=059e59d366d2411e94374ac6bbf0f75f&limit=1"
+    url = "http://api.giphy.com/v1/gifs/random?tag=#{search_term}&api_key=059e59d366d2411e94374ac6bbf0f75f&limit=5"
     resp = Net::HTTP.get_response(URI.parse(url))
     buffer = resp.body
     result = JSON.parse(buffer)
-    image_path = result["data"][0]["images"]["fixed_height"]["url"]
+    image_path = result["data"]["image_url"]
     category = Category.find_or_create_by(name: search_term)
     @gif = Gif.create(search_term: search_term, image_path: image_path, category_id: category.id)
     redirect_to gifs_path
